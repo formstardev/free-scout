a7000c363e70a9b44b610f441234663bcfe135d0
WonderfulDev <abandonraj@gmail.com>
2018-09-21 16:10:58 +1000

    Allow the user to override default days parameter on the command line

--- app/Misc/Helper.php
+++ app/Misc/Helper.php
@@ -136,13 +136,9 @@ class Helper
                     if (in_array($current_route, $secondary_routes)) {
                         return ($secondary_name == $menu_item_name || $primary_name == $menu_item_name);
                     }
-                } elseif (is_string($secondary_name)) {
-                    if ($current_route == $secondary_routes) {
-                        return ($secondary_name == $menu_item_name || $primary_name == $menu_item_name);
-                    }
                 } else {
                     if ($current_route == $secondary_routes) {
-                        return ($primary_name == $menu_item_name);
+                        return ($secondary_name == $menu_item_name || $primary_name == $menu_item_name);
                     }
                 }
             }
