3eb4e2497a2962dff1c10d3179aa8e1ff335f084
WonderfulDev <abandonraj@gmail.com>
2018-11-12 05:23:19 -0800

    Open tracking tuning

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Conversation;
 use App\Option;
+use App\Thread;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -104,4 +106,31 @@ class PublicController extends Controller
 
         return redirect()->route('dashboard');
     }
+
+    /*
+     * Set a thread as read by customer
+     */
+    public function setThreadAsRead($conversation_id, $thread_id) {
+        $conversation = Conversation::findOrFail($conversation_id);
+        $thread       = Thread::findOrFail($thread_id);
+
+        // We only track the first opening
+        if (empty($thread->opened_at)) {
+            $thread->opened_at = date('Y-m-d H:i:s');
+            $thread->save();
+        }
+
+        // Create a 1x1 ttransparent pixel and return it
+        $pixel = sprintf('%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%', 71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 255, 0, 192, 192, 192, 0, 0, 0, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59);
+        $response = \Response::make($pixel, 200);
+        $response->header('Content-type', 'image/gif');
+        $response->header('Content-Length', 42);
+        $response->header('Cache-Control', 'private, no-cache, no-cache=Set-Cookie, proxy-revalidate');
+        $response->header('Expires', 'Wed, 11 Jan 2000 12:59:00 GMT');
+        $response->header('Last-Modified', 'Wed, 11 Jan 2006 12:59:00 GMT');
+        $response->header('Pragma', 'no-cache');
+
+        return $response;
+    }
+
 }
