b497186713751e2dd1106f42167e0785acd6d7c7
WonderfulDev <abandonraj@gmail.com>
2019-05-06 02:47:45 -0700

    customer.profile_data hook

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,8 +5,6 @@ namespace App\Console;
 use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Misc\Mail;
-use App\Option;
 
 class Kernel extends ConsoleKernel
 {
@@ -75,30 +73,10 @@ class Kernel extends ConsoleKernel
         }
 
         // Fetch emails from mailboxes
-        $fetch_command = $schedule->command('freescout:fetch-emails')
+        $schedule->command('freescout:fetch-emails')
+            ->everyMinute()
             ->withoutOverlapping()
             ->sendOutputTo(storage_path().'/logs/fetch-emails.log');
-
-        switch (Option::get('fetch_schedule')) {
-            case Mail::FETCH_SCHEDULE_EVERY_MINUTE:
-                $fetch_command->everyMinute();
-                break;
-            case Mail::FETCH_SCHEDULE_EVERY_FIVE_MINUTES:
-                $fetch_command->everyFiveMinutes();
-                break;
-            case Mail::FETCH_SCHEDULE_EVERY_TEN_MINUTES:
-                $fetch_command->everyTenMinutes();
-                break;
-            case Mail::FETCH_SCHEDULE_EVERY_FIFTEEN_MINUTES:
-                $fetch_command->everyFifteenMinutes();
-                break;
-            case Mail::FETCH_SCHEDULE_EVERY_THIRTY_MINUTES:
-                $fetch_command->everyThirtyMinutes();
-                break;
-            case Mail::FETCH_SCHEDULE_HOURLY:
-                $fetch_command->Hourly();
-                break;
-        }
 
         $schedule = \Eventy::filter('schedule', $schedule);
 
