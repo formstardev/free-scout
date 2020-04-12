d3b0e02efaf525d55cef5371a7740aba23ef32a0
WonderfulDev <abandonraj@gmail.com>
2020-04-12 12:23:56 +0200

    Use getCompanyName() wrapper for fallback value

--- app/Console/Commands/FetchEmails.php
+++ app/Console/Commands/FetchEmails.php
@@ -114,7 +114,6 @@ class FetchEmails extends Command
             $imap_folders = $mailbox->getInImapFolders();
 
             foreach ($imap_folders as $folder_name) {
-                $folder = null;
                 try {
                     $folder = $client->getFolder($folder_name);
                 } catch (\Exception $e) {
