3ddf9bee3f9caa05d8f2e2a633e8c4643c0adf5f
WonderfulDev <abandonraj@gmail.com>
2022-07-25 15:58:45 +1000

    Fix for #2111 removed trailing slash from tracking pixel route

--- app/Console/Commands/FetchEmails.php
+++ app/Console/Commands/FetchEmails.php
@@ -134,27 +134,28 @@ class FetchEmails extends Command
         $folders = [];
 
         // Fetch emails from custom IMAP folders.
-        //if ($mailbox->in_protocol == Mailbox::IN_PROTOCOL_IMAP) {
-        $imap_folders = $mailbox->getInImapFolders();
+        if ($mailbox->in_protocol == Mailbox::IN_PROTOCOL_IMAP) {
+            $imap_folders = $mailbox->getInImapFolders();
 
-        foreach ($imap_folders as $folder_name) {
-            $folder = null;
-            try {
-                $folder = $client->getFolder($folder_name);
-            } catch (\Exception $e) {
-                // Just log error and continue.
-                $this->error('['.date('Y-m-d H:i:s').'] Could not get mailbox IMAP folder: '.$folder_name);
-            }
+            foreach ($imap_folders as $folder_name) {
+                $folder = null;
+                try {
+                    $folder = $client->getFolder($folder_name);
+                } catch (\Exception $e) {
+                    // Just log error and continue.
+                    $this->error('['.date('Y-m-d H:i:s').'] Could not get mailbox IMAP folder: '.$folder_name);
+                }
 
-            if ($folder) {
-                $folders[] = $folder;
+                if ($folder) {
+                    $folders[] = $folder;
+                }
             }
+            // try {
+            //     //$folders = $client->getFolders();
+            // } catch (\Exception $e) {
+            //     // Do nothing
+            // }
         }
-        // try {
-        //     //$folders = $client->getFolders();
-        // } catch (\Exception $e) {
-        //     // Do nothing
-        // }
 
         $unseen = \Eventy::filter('fetch_emails.unseen', $this->option('unseen'), $mailbox);
         if ($unseen != $this->option('unseen')) {
